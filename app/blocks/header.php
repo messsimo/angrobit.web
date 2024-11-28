294f0c70bd2e3770431096954862ceb41aa4b0ab
Daniel Mihai <danikmihai23@mail.ru>
2024-11-28 13:22:28 +0200

    Feature: Header template

---
+++ app/blocks/header.php
@@ -0,0 +1,34 @@
+<!-- Header block -->
+<header>
+    <div class="right-container">
+        <div class="logo-name">
+            <img src="/public/src/images/gamepad.png">
+            <h2>Products</h2>
+        </div>
+
+        <div class="dropmenu">
+            <a>About Us</a>
+            <p>Referal</p>
+        </div>
+    </div>
+
+    <div class="logo">
+        <img src="/public/src/images/logo.png" alt="AngroBit">
+        <div class="sub-block"></div>
+    </div>
+
+    <div class="left-container">
+        <div class="language">
+            <div class="text">
+                <div class="color"></div>
+                <p>A <b>中</b></p>
+            </div>
+            <img src="/public/src/images/arrow_down.png" alt="AngroBit">
+        </div>
+
+        <div class="btns">
+            <a href="">Shop</a>
+            <a href="">Sign In</a>
+        </div>
+    </div>
+</header>
--- app/views/main.php
+++ app/views/main.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Connecting CSS -->
+    <link rel="stylesheet" href="/public/src/css/main.css">
     <title>AngroBit</title>
 </head>
 <body>
-    
+    <!-- Conntecting header -->
+    <?php 
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/app/blocks/header.php';
+    ?>
 </body>
 </html>
