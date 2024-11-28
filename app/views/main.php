4319a3e814eaa31be3d2fdfcbb7bdb9811f94c49
Daniel Mihai <danikmihai23@mail.ru>
2024-11-28 13:36:18 +0200

    Feature: Hero template

--- app/views/main.php
+++ app/views/main.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Connecting CSS -->
+    <link rel="stylesheet" href="/public/src/css/header.css">
     <link rel="stylesheet" href="/public/src/css/main.css">
     <title>AngroBit</title>
 </head>
@@ -12,5 +13,18 @@
     <?php 
         require_once $_SERVER['DOCUMENT_ROOT'] . '/app/blocks/header.php';
     ?>
+
+    <!-- Hero block -->
+    <div class="hero">
+        <div class="text">
+            <h2>Modern agriculture </h2>
+            <h3>starts here</h3>
+        </div>
+
+        <div class="shops">
+            <img src="/public/src/images/app_store.png" alt="AppStore">
+            <img src="/public/src/images/google_play.png" alt="Google Play">
+        </div>
+    </div>
 </body>
 </html>
