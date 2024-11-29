4761c650278f9c5042a826ca1d92b0aece3bb8ad
Daniel Mihai <danikmihai23@mail.ru>
2024-11-29 07:15:33 +0200

    Feature: News template

--- app/views/main.php
+++ app/views/main.php
@@ -34,15 +34,52 @@
             <div class="nav">
                 <ul>
                     <li><a href="#">Game</a></li>
+                    <img src="public/src/images/border_bottom.png">
                     <li><a href="#">Mobile App</a></li>
+                    <img src="public/src/images/border_bottom.png">
                     <li><a href="#">Platform</a></li>
+                    <img src="public/src/images/border_bottom.png">
                     <li><a href="#">Shop</a></li>
+                    <img src="public/src/images/border_bottom.png">
                 </ul>
             </div>
         </div>
         <div class="bottom-block"></div>
     </div>
 
-    <!--  -->
+    <!-- News block -->
+    <div class="news">
+        <img class="deco" src="/public/src/images/deco_news.png">
+        <div class="block-name">
+            <img src="/public/src/images/puls_left.png">
+            <h2>News & Updates</h2>
+            <img src="/public/src/images/puls_right.png">
+        </div>
+
+        <div class="container">
+            <div class="block">
+                <img src="/public/src/images/info.png">
+                <img src="/public/src/images/facebook.png">
+                <img src="/public/src/images/youtube.png">
+                <img src="/public/src/images/twitter.png">
+            </div>
+
+            <div class="block-center">
+                <img src="/public/src/images/news_block_top.png">
+                <img  class="img" src="/public/src/images/img.png">
+                <a href=""></a>
+                    <img src="/public/src/images/block_news_bottom.png">
+                </a>
+            </div>
+
+            <div class="block">
+                <img src="/public/src/images/tiktok.png">
+                <img src="/public/src/images/reddit.png">
+                <img src="/public/src/images/discord.png">
+                <img src="/public/src/images/instagram.png">
+            </div>
+        </div>
+        <img class="deco deco-bottom" src="/public/src/images/deco_news.png">
+    </div>
 </body>
 </html>
