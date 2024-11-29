f2485159e89f5a796dce909821c8b1d1c521da67
Daniel Mihai <danikmihai23@mail.ru>
2024-11-29 06:22:47 +0200

    Feature: Menu template

--- app/views/main.php
+++ app/views/main.php
@@ -26,5 +26,23 @@
             <img src="/public/src/images/google_play.png" alt="Google Play">
         </div>
     </div>
+
+    <!-- Menu block -->
+    <div class="menu">
+        <div class="top-block"></div>
+        <div class="container">
+            <div class="nav">
+                <ul>
+                    <li><a href="#">Game</a></li>
+                    <li><a href="#">Mobile App</a></li>
+                    <li><a href="#">Platform</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottom-block"></div>
+    </div>
+
+    <!--  -->
 </body>
 </html>
